cd4e81c5b4323e1363d53d9fe4821ff47360d630
drowe4 <32925729+drowe4@users.noreply.github.com>
2019-10-30 10:49:41 +0000

    Update ludo.py

--- ludo.py
+++ ludo.py
@@ -2,11 +2,12 @@
 
 
 class Player:
-    def __init__(self, id, colour, name):
-        self.id = id
+    def __init__(self, player_id, colour, name, board): 
+        self.player_id = player_id
         self.colour = colour
         self.name = name
-        self.piece = [Piece()] * 4
+        self.board = board
+        self.piece = [Piece(self, i) for i in range(4)]
 
     def play():
         #plays his turn...
@@ -15,32 +16,50 @@ class Player:
             #if rolls < 6
                 #game logic
         pass
-
+    
+    def get_player_id(self):
+        return self.player_id
+    
     def quit():
         quit()
-        pass
     
 
-class Path:
+class Step:
     
-    def __init__(self):
-        pass
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
     
-    def next():
-        # step through path
+    def look_forward(self, n):
+        # look forward n steps. Returns False if n > steps left.
         pass
     
     
 class Piece:
 
-    def __init__(self, owner, position, path):
+    def __init__(self, owner, piece_id):
         self.owner = owner
-        self.position = position #what data struct is position?
-        self.path = self.generate_path()
+        self.piece_id = piece_id
+        self.step = self.generate_path(owner.board, piece_id)
 
-    def generate_path(self):
-        # generates the path of a piece
-        pass
+    def generate_path(self, board, piece_id):
+        player_id = self.owner.get_player_id()
+        i = 2 + player_id * 13
+        j = 0
+        p = Step(board.board[0][player_id][piece_id])
+        start = p
+        while j < board.players:
+            p.next = Step(board.board[1][i])
+            p = p.next
+            j += 1
+            i = (i + 1) % 52
+
+        j = 0
+        while j < 5:
+            p.next = Step(board.board[2][player_id][j])
+            p = p.next
+            j += 1
+        return start
 
 
 class Square:
@@ -56,16 +75,20 @@ class Board:
         self.colours = colours
         self.board = self.make_board(players)
 
-    def make_board(players):
-        b = []
+    def make_board(self, players):
+        b = [[[Square("start_square") for i in range(4)] for j in range(players)]]
+        
+        b.append([Square("general_square") for i in range(13) for j in range(players)])
 
-        for i in range(players):
-            p = []
-            for j in range(3):
-                p.append([Square(i)] * 6)
-            b.append(p)
+        b.append([[Square("end_squares") for i in range(5)] for j in range(players)])
+        
         return b
-    
+
+    def set_board():
+        for i in players:
+            pass
+
+        
 class Die:
 
     def __init__(self, sides=6):
@@ -76,8 +99,8 @@ class Die:
 
         pass
 
+
 def main():
-    #set up game state
     pass
 
 if __name__ == "__main__":
